60bc88de35a4dde46adacaefe3b15940c4b2ea0c
Ruhul Amin <ruhulamin.et15@gmail.com>
2024-08-08 17:45:31 +0600

    update handle error

--- src/services/index.js
+++ src/services/index.js
@@ -1,14 +1,18 @@
 const createError = require("http-errors");
 const Users = require("../models/usersModel");
+const { default: mongoose } = require("mongoose");
 
 const findUserById = async (id, options = {}) => {
   try {
     const user = await Users.findById(id, options);
     if (!user) {
-      throw createError(404, "user not found");
+      throw createError(404, "user not found with this id");
     }
     return user;
   } catch (error) {
+    if (error instanceof mongoose.Error.CastError) {
+      throw createError(404, "user not found!");
+    }
     throw error;
   }
 };
